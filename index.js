56a83cf5fd5c42d1df87c80359cf68144e8bc925
Simon <simja649@student.liu.se>
2020-11-10 23:47:04 +0100

    now added with haters

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+let names = ["Axel Boström", "Sean"]
 
 client.login(config.token);
 
 client.on('message', async message => {
   if (!message.guild) return;
 
-  if (message.content === 'grävmaskin') {
 
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join();
+
+      if (message.content === 'grävmaskin') {
       const dispatcher = connection.play(__dirname+ '/Gravmaskin.m4a');
-    } else {
+      }
+      
+      if (message.content === "Axel Boström" || message.content === "Sean") {
+        const dispatcher = connection.play(__dirname + '/Waaa.m4a');
+      }
+      
+    } 
+ 
+      else {
       message.reply('Grävmaskin hittar inget häng');
-    }
-  }
+      }
+
 });
 
 client.on('ready', discordOn);
