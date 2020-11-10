99ccbef129812cd23857c146db6416e6bd932d79
Simon <simja649@student.liu.se>
2020-11-10 22:57:25 +0100

    varför gör jag detta mot mig själv

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.login(config.token);
+
+client.on('message', async message => {
+  if (!message.guild) return;
+
+  if (message.content === 'grävmaskin') {
+
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.play('/Users/simonjakobsson/Documents/gravmaskindiscord/Gravmaskin.m4a');
+    } else {
+      message.reply('Grävmaskin hittar inget häng');
+    }
+  }
+});
+
+client.on('ready', discordOn);
+
+function discordOn(){
+    console.log("yes");
+}
+
