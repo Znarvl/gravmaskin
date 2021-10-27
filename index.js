6dc1e7fa56eac23b95292b5acc4c3ae7292bbb42
Simon Jakobsson <31953373+Znarvl@users.noreply.github.com>
2021-10-27 17:43:49 +0200

    Fuck you github and this works

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-let names = ["Axel Boström", "Sean"]
+let names = ["Axel", "Sean"]
+
 
 client.login('Nzc1ODAxOTU1NDAyNTE0NDUz.X6rn3w.wv93ikOl - QILC6mHlaEzN1mRw4Y');
 
 client.on('message', async message => {
-  if (!message.guild) return;
 
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join();
@@ -13,17 +13,19 @@ client.on('message', async message => {
       if (message.content.includes('Grävmaskin')) {
       const dispatcher = connection.play(__dirname+ '/Gravmaskin.m4a');
       }
-      
-      if (message.content.includes("Axel Boström")|| message.content.includes("Sean")) {
-        const dispatcher = connection.play(__dirname + '/Waaa.m4a');
-      }
-      
-    } 
-});
+      names.forEach(name => {
+          if (message.content.includes(name)) {
+            const dispatcher = connection.play(__dirname + '/Waaa.m4a');
+        }
+    })
+    if (message.content.includes("leave")) {
+      connection.disconnect()
+    }
+}});
 
 client.on('ready', discordOn);
 
 function discordOn(){
     console.log("yes");
 }
-
+ 
